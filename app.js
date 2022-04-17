fcbe4cbdbf9c23fd42d23b9736280f91192b6eda
softtoday72 <demonhunter720525@hotmail.com>
2022-04-17 23:40:04 +0800

    add login logout create edit delete

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const app = express()
 const port = 3000
+
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
+const routes = require('./routes')
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+const flash = require('connect-flash')
+
 app.engine('hbs', exphbs({defaultLayout:'main', extname:'.hbs'}))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
+app.use(flash())
 
-
-app.get('/', (req, res) => {
-  res.send('hello world')
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
 })
 
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-app.post('/users/register', (req, res) => {
-  res.send('register')
-})
-
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
